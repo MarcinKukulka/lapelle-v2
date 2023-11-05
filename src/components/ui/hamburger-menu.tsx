e2e22d6f102ced1e90d488b65b1a31eaac4a33ca
Marcin <marcinkukulka@gmail.com>
2023-11-05 21:15:41 +0100

    refactor: hamburger menu

--- src/components/ui/hamburger-menu.tsx
+++ src/components/ui/hamburger-menu.tsx
@@ -1,27 +1,30 @@
-export const HamburgerMenu = ({
-  isOpen,
-  onClick,
-}: {
-  isOpen: boolean;
-  onClick: () => void;
-}) => {
-  const genericHamburgerLine = `h-1 w-10 my-1 rounded-full bg-gold transition ease transform duration-300`;
-  return (
-    <button
-      className="flex flex-col h-12 w-12 justify-center items-center"
-      onClick={onClick}
-    >
-      <div
-        className={`${genericHamburgerLine} ${
-          isOpen && 'rotate-45 translate-y-3 '
-        }`}
-      />
-      <div className={`${genericHamburgerLine} ${isOpen && 'opacity-0'}`} />
-      <div
-        className={`${genericHamburgerLine} ${
-          isOpen && '-rotate-45 -translate-y-3 '
-        }`}
-      />
-    </button>
-  );
+import type { ComponentPropsWithoutRef } from 'react';
+import { cn } from '@/lib/utils';
+type HamburgerMenuProps = {
+	isOpen: boolean;
+} & ComponentPropsWithoutRef<'button'>;
+
+export const HamburgerMenu = ({ isOpen, ...props }: HamburgerMenuProps) => {
+	const genericHamburgerLine = `h-1 w-10 my-1 rounded-full bg-gold transition ease transform duration-300`;
+	return (
+		<button
+			className="flex h-12 w-12 flex-col items-center justify-center"
+			// onClick={onClick}
+			{...props}
+		>
+			<div
+				className={cn(
+					genericHamburgerLine,
+					isOpen && 'translate-y-3 rotate-45 ',
+				)}
+			/>
+			<div className={cn(genericHamburgerLine, isOpen && 'opacity-0')} />
+			<div
+				className={cn(
+					genericHamburgerLine,
+					isOpen && '-translate-y-3 -rotate-45 ',
+				)}
+			/>
+		</button>
+	);
 };
