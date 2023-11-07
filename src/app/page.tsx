33a13c8743bc157ea1c643e451f70ea2d172f534
Marcin <marcinkukulka@gmail.com>
2023-11-07 10:55:05 +0100

    feat: added map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { GoogleMap } from '@/components/google-map';
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col">
+			<GoogleMap />
+		</main>
+	);
 }
---
+++ src/components/google-map.tsx
@@ -0,0 +1,12 @@
+export const GoogleMap = () => {
+	return (
+		<section>
+			<iframe
+				className="h-[40vh] w-full"
+				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2564.077567910527!2d20.982975515765474!3d50.00990132689097!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473d8537680e0f9b%3A0xc5bf3c05cc431c9c!2sLa%20Pelle%20Nowoczesna%20Kosmetologia%20Agnieszka%20Tryli%C5%84ska!5e0!3m2!1spl!2spl!4v1624455351818!5m2!1spl!2spl"
+				loading="lazy"
+				title="Mapa do salonu"
+			></iframe>
+		</section>
+	);
+};
