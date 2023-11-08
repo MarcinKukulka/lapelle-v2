94826097fe5f2a3930f222be9b43d7a767aac120
Marcin <marcinkukulka@gmail.com>
2023-11-08 22:25:52 +0100

    fix: colors display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { GoogleMap } from '@/components/google-map';
-import { Heading } from '@/components/ui/heading';
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col">
-			<Heading text="Oferta" color="white" />
 			<GoogleMap />
 		</main>
 	);
--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -4,22 +4,27 @@ import { cn } from '@/lib/utils';
 const playfair = Playfair_Display({ subsets: ['latin'] });
 
 type HeadingProps = {
-	color: string;
+	textColor: string;
+	lineColor: string;
 	text: string;
 };
 
-export const Heading = ({ color, text }: HeadingProps) => {
+export const Heading = ({ textColor, lineColor, text }: HeadingProps) => {
+	const HeadingLine = (
+		<span className={cn(`block h-2 w-2/5 border-b-2 `, lineColor)} />
+	);
 	return (
 		<div className="flex items-center justify-center py-8">
-			<span className={`block h-2 w-2/5 border-b-2 border-${color}`} />
+			{HeadingLine}
 			<h2
 				className={cn(
-					`${playfair.className} text-${color} px-8 text-4xl font-semibold`,
+					`${playfair.className} } px-8 text-4xl font-semibold`,
+					textColor,
 				)}
 			>
 				{text}
 			</h2>
-			<span className={`block h-2 w-2/5 border-b-2 border-${color}`} />
+			{HeadingLine}
 		</div>
 	);
 };
