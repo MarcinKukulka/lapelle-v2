6fbf5fb267d108ae894176f8c4d54e0a4ce86e4c
Marcin <marcinkukulka@gmail.com>
2023-11-15 21:42:07 +0100

    feat: added offers section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Carousel } from '@/components/carousel';
 import { GoogleMap } from '@/components/google-map';
+import { Offers } from '@/components/offers';
 
 export default function Home() {
 	return (
 		<main>
 			<Carousel />
+			<Offers />
 			<GoogleMap />
 		</main>
 	);
---
+++ src/components/offers.tsx
@@ -0,0 +1,9 @@
+import { Heading } from './ui/heading';
+
+export const Offers = () => {
+	return (
+		<section>
+			<Heading textColor="text-black" lineColor="border-black" text="Oferta" />
+		</section>
+	);
+};
