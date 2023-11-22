f36fc2b28e6c74245f243a4cb95b40e509095964
Marcin <marcinkukulka@gmail.com>
2023-11-22 16:25:59 +0100

    feat: reading images from folder

--- src/components/galllery.tsx
+++ src/components/galllery.tsx
@@ -1,41 +1,30 @@
 import Image from 'next/image';
 import { Heading } from '@/ui/heading';
-import Image1 from '@/assets/gallery/galeria1-desktop.jpg';
-import Image2 from '@/assets/gallery/galeria2-desktop.jpg';
-import Image3 from '@/assets/gallery/galeria3-desktop.jpg';
-import Image4 from '@/assets/gallery/galeria4-desktop.jpg';
-import Image5 from '@/assets/gallery/galeria5-desktop.jpg';
-import Image6 from '@/assets/gallery/galeria6-desktop.jpg';
-import Image7 from '@/assets/gallery/galeria7-desktop.jpg';
-import Image8 from '@/assets/gallery/galeria8-desktop.jpg';
-import Image9 from '@/assets/gallery/galeria9-desktop.jpg';
+import { readImagesFromGallery } from '@/lib/utils';
 
-const IMAGES = [
-	Image1,
-	Image2,
-	Image3,
-	Image4,
-	Image5,
-	Image6,
-	Image7,
-	Image8,
-	Image9,
-];
+const imagesPath: string = 'src/assets/gallery';
+
+type GalleryImages = {
+	src: string;
+	alt: string;
+};
+
+const images: GalleryImages[] = readImagesFromGallery(imagesPath);
 
 export const Gallery = () => {
 	return (
 		<section className="mx-auto my-8 max-w-screen-xl">
 			<Heading lineColor="border-black" text="Galeria" textColor="text-black" />
 			<div className="mt-8 grid grid-cols-1 justify-items-center gap-8 px-8 sm:grid-cols-2 lg:grid-cols-4">
-				{IMAGES.map((image, index) => {
+				{images.map(({ src, alt }, index) => {
 					return (
 						<Image
 							className="h-64 object-cover transition-transform duration-300 hover:scale-110"
 							height={200}
 							width={300}
-							src={image}
+							src={src}
 							key={index}
-							alt="Zdjęcie z galerii"
+							alt={alt}
 						/>
 					);
 				})}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
+import fs from 'fs';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+	return twMerge(clsx(inputs));
 }
+
+export const readImagesFromGallery = (folderPath: string) => {
+	const images = fs.readdirSync(folderPath);
+
+	const processedImages = images.map((image: string) => {
+		return {
+			src: `/../assets/gallery/${image}`,
+			alt: 'Zdjęcie z galerii',
+		};
+	});
+
+	return processedImages;
+};
