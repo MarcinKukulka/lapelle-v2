70121d64fe1b26dbf907d99d8f1878df1d9053f8
Marcin <marcinkukulka@gmail.com>
2023-12-09 21:31:38 +0100

    style: fix mobile view in contatc page

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,14 +1,17 @@
 import { GoogleMap } from '@/components/google-map';
 import { ContactInfo } from '@/components/ui/contact-info';
 import { ContactOpeningHours } from '@/components/ui/contact-opening-hours';
+import { SectionWrapper } from '@/components/ui/section-wrapper';
 
 export default function Kontakt() {
 	return (
-		<section>
-			<div className="flex flex-col justify-between bg-black px-10 pt-12 md:flex-row">
-				<ContactInfo />
-				<ContactOpeningHours />
-			</div>
+		<section className="bg-black">
+			<SectionWrapper>
+				<div className="flex flex-col justify-between px-10 pt-12 md:flex-row">
+					<ContactInfo />
+					<ContactOpeningHours />
+				</div>
+			</SectionWrapper>
 			<GoogleMap />
 		</section>
 	);
