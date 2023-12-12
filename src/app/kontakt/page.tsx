4f85784cbfef3d5d3141cf7af075f1e8eab99afd
Marcin <marcinkukulka@gmail.com>
2023-12-12 20:32:21 +0100

    feat: crated contact form

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,3 +1,4 @@
+import { ContactForm } from '@/components/contact-form';
 import { GoogleMap } from '@/components/google-map';
 import { ContactInfo } from '@/components/ui/contact-info';
 import { ContactOpeningHours } from '@/components/ui/contact-opening-hours';
@@ -6,6 +7,9 @@ import { SectionWrapper } from '@/components/ui/section-wrapper';
 export default function Kontakt() {
 	return (
 		<section className="bg-black">
+			<div className="flex justify-center  bg-white py-20">
+				<ContactForm />
+			</div>
 			<SectionWrapper>
 				<div className="flex flex-col justify-between px-10 pt-12 md:flex-row">
 					<ContactInfo />
