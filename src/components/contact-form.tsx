be990b814cac2712d43e8a1cc48029b4ec3b7727
Marcin <marcinkukulka@gmail.com>
2023-12-13 18:29:13 +0100

    update: contact form

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -4,11 +4,11 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 
-import { Button } from '@/components/ui/button';
+import { Textarea } from '@/ui/textarea';
+import { Button } from '@/ui/button';
 import {
 	Form,
 	FormControl,
-	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
@@ -18,7 +18,16 @@ import { Input } from '@/components/ui/input';
 
 const formSchema = z.object({
 	username: z.string().min(2, {
-		message: 'Username must be at least 2 characters.',
+		message: 'Imię musi mieć co najmniej 2 znaki.',
+	}),
+	email: z.string().email({
+		message: 'E-mail musi być poprawny.',
+	}),
+	subject: z.string().min(5, {
+		message: 'Temat wiadomości musi mieć co najmniej 5 znaków.',
+	}),
+	message: z.string().min(10, {
+		message: 'Wiadomość musi mieć co najmniej 10 znaków.',
 	}),
 });
 
@@ -27,6 +36,9 @@ export const ContactForm = () => {
 		resolver: zodResolver(formSchema),
 		defaultValues: {
 			username: '',
+			email: '',
+			subject: '',
+			message: '',
 		},
 	});
 
@@ -37,29 +49,95 @@ export const ContactForm = () => {
 		console.log(values);
 	}
 
+	const resetForm = () => {
+		form.reset();
+	};
+
 	return (
 		<Form {...form}>
 			<form
 				onSubmit={form.handleSubmit(onSubmit)}
-				className="max-w-screen-xl space-y-8"
+				className="w-2/3 max-w-screen-xl space-y-4 md:w-1/3"
 			>
 				<FormField
 					control={form.control}
 					name="username"
 					render={({ field }) => (
 						<FormItem>
-							<FormLabel>Username</FormLabel>
+							<FormLabel>Imię</FormLabel>
 							<FormControl>
-								<Input placeholder="shadcn" {...field} />
+								<Input
+									className="bg-gold/10"
+									placeholder="Wpisz swoje imię"
+									{...field}
+								/>
 							</FormControl>
-							<FormDescription>
-								This is your public display name.
-							</FormDescription>
 							<FormMessage />
 						</FormItem>
 					)}
 				/>
-				<Button type="submit">Submit</Button>
+				<FormField
+					control={form.control}
+					name="email"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>E-mail</FormLabel>
+							<FormControl>
+								<Input
+									className="bg-gold/10"
+									placeholder="Podaj swój adres e-mail"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="subject"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Temat</FormLabel>
+							<FormControl>
+								<Input
+									className="bg-gold/10"
+									placeholder="Podaj temat swojej wiadomości"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="message"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Wiadomość</FormLabel>
+							<FormControl>
+								<Textarea
+									className="bg-gold/10"
+									placeholder="Napisz swoją wiadomość"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<div className="flex justify-between">
+					<Button type="submit">Wyślij</Button>
+					<Button
+						variant="ghost"
+						className="underline underline-offset-4 hover:bg-gold/40"
+						type="reset"
+						onClick={resetForm}
+					>
+						Wyczyść formularz
+					</Button>
+				</div>
 			</form>
 		</Form>
 	);
