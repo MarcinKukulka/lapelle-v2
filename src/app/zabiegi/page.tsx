9d2300aa8dc639ba7148430430289d416da46518
Marcin <marcinkukulka@gmail.com>
2023-12-14 23:54:33 +0100

    feat: connect with hygraph

--- src/app/zabiegi/page.tsx
+++ src/app/zabiegi/page.tsx
@@ -1,9 +1,19 @@
+import { notFound } from 'next/navigation';
+import { executeGraphql } from '@/api/grapqhlApi';
 import { TreatmentArticle } from '@/components/ui/treatment-article';
 import { TreatmentList } from '@/components/ui/treatment-list';
+import { TreatmentsGetListDocument } from '@/gql/graphql';
 
-export default function Zabiegi() {
+export default async function Zabiegi() {
+	const { treatments } = await executeGraphql(TreatmentsGetListDocument);
+	if (!treatments) {
+		notFound();
+	}
+	console.log(treatments);
+
 	return (
 		<div className="mx-auto flex max-w-screen-xl">
+			<pre>{JSON.stringify(treatments, null, 2)}</pre>
 			<TreatmentList />
 			<TreatmentArticle />
 		</div>
