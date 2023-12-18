baea4d899541edbffef088769a808a8ecb47c0fe
Marcin <marcinkukulka@gmail.com>
2023-12-18 23:53:51 +0100

    feat: added price list page

---
+++ src/app/cennik/page.tsx
@@ -0,0 +1,8 @@
+import { Heading } from '@/components/ui/heading';
+export default function PriceListPage() {
+	return (
+		<section>
+			<Heading text="Cennik" lineColor="border-black" />
+		</section>
+	);
+}
