e3c16f35dc06515ee4e387603a34a6af153f6c79
Marcin <marcinkukulka@gmail.com>
2024-01-18 21:48:41 +0100

    refactor: remove unused code from carousel

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from 'next/image';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import carouselImage1 from '@/assets/carousel/dermobrazja.jpg';
 import carouselImage2 from '@/assets/carousel/header1.jpg';
 import carouselImage3 from '@/assets/carousel/voucher_podarunkowy.jpg';
@@ -11,12 +11,9 @@ const carouselImages = [
 	{ image: carouselImage2, url: '', alt: '2' },
 	{ image: carouselImage3, url: '', alt: '3' },
 ];
-// million-ignore
 
 export const Carousel = () => {
 	const [currentSlide, setCurrentSlide] = useState(0);
-	const [touchPosition, setTouchPosition] = useState<number | null>(null);
-	const carouselRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
 		const interval = setInterval(() => {
@@ -25,37 +22,10 @@ export const Carousel = () => {
 			} else {
 				setCurrentSlide(currentSlide + 1);
 			}
-		}, 4000);
+		}, 3000);
 
 		return () => clearInterval(interval);
 	}, [currentSlide]);
-
-	const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
-		if (e.touches && e.touches.length) {
-			setTouchPosition(e.touches[0].clientX);
-		}
-	};
-
-	const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
-		if (touchPosition === null || !carouselRef.current) {
-			return;
-		}
-
-		const currentX = e.touches[0].clientX;
-		const difference = touchPosition - currentX;
-
-		if (difference > 5) {
-			carouselRef.current.scrollLeft += difference;
-		} else if (difference < -5) {
-			carouselRef.current.scrollLeft -= -difference;
-		}
-
-		setTouchPosition(currentX);
-	};
-
-	const handleTouchEnd = () => {
-		setTouchPosition(null);
-	};
 
 	const handleNextSlide = () => {
 		if (currentSlide === carouselImages.length - 1) {
@@ -80,10 +50,6 @@ export const Carousel = () => {
 		<div className="mx-auto">
 			<div className="relative overflow-hidden">
 				<div
-					onTouchStart={handleTouchStart}
-					onTouchMove={handleTouchMove}
-					onTouchEnd={handleTouchEnd}
-					ref={carouselRef}
 					className="flex snap-x snap-mandatory transition-transform duration-500 ease-in-out"
 					style={{ transform: `translateX(-${currentSlide * 100}%)` }}
 				>
